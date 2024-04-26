3c074a996fd02b766c4d4e5b461b47cbc3d0e49c
brunapcosta <brunaa.jornalismo@gmail.com>
2024-04-26 16:07:05 -0300

    linkedin-logo

--- src/Contairner/Contact/styles.js
+++ src/Contairner/Contact/styles.js
@@ -42,7 +42,7 @@ export const SocialMedias = styled.div`
         margin-left: 130px;
         margin-top: 54px;
         background-color: transparent;
-        filter: opacity(0.5) drop-shadow(0 0 0 white);
+        filter: opacity(0.5) drop-shadow(0 0 0 blue);
     }
 
     .github{
